42a0e3066201cb60bf4e6b183d297138f52a4250
CBT <CBT@DESKTOP-R9RU20I>
2019-09-27 13:12:38 -0500

    //se agrgo la clase emp y metodo

---
+++ TestProjectJava/src/org/ejemplo/modelo/Ejemplo.java
@@ -0,0 +1,32 @@
+package org.ejemplo.modelo;
+
+public class Ejemplo {
+	
+	private int id;
+	private String nombre;
+	
+	public static void main (String [] args) {
+		System.out.println("EjemploGIT");
+		
+	}
+	
+	
+	
+	//GETTERS AND SETTERS 
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+
+}
